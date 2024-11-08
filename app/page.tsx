ab46ae54bcb4396999c26a52f2c34aab2404817f
IsraelPontes <israelcpontes@hotmail.com>
2024-11-07 23:37:04 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home =() =>{
-  return <h1 className="text-red-500">Hellô World</h1>;
+  return <Button>Hello</Button> 
 };
 
 export default Home;
