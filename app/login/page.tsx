5cd3a577e0d46c535ec37c9b1b16a2e4d59d923c
IsraelPontes <israelcpontes@hotmail.com>
2024-11-09 16:08:05 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,37 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async() => {
+    const { userId } = await auth();
+    if (userId) {
+        redirect("/");
+    }
   return (
-    <div className="grid grid-cols-2">
-      <Image 
-      src="/logo.vsg" 
-      width={173} 
-      height={39} 
-      alt="Finance AI" 
-      className="mb-8"
+    <div className="grid grid-cols-2 h-full">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+      <Image
+        src="/logo.svg"
+        width={173}
+        height={39}
+        alt="Finance AI"
+        className="mb-8 object-cover"
       />
-      <div className="flex flex-col h-full justify-center mx-auto max-w-[550px] p-8">
-      <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
-      <p className="text-muted-foreground">
-        A Fincance AI é uma platarma de gestão finaceira que utiliza IA para
-        monitorar suas movimentações, e oferecer insights personalizados,
-        facilitando o controle do seu orçamento.
-      </p>
-      <Button variant="outline">
-        <LogInIcon className="mr-2">
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="text-muted-foreground">
+          A Fincance AI é uma platarma de gestão finaceira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2"></LogInIcon>
             Fazer login ou criar conta
-        </LogInIcon>
-      </Button>
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image
